24ccda9c4758495585955c5a0f67d273e6e99a53
Odeh Adejoh <42589643+stradox4u@users.noreply.github.com>
2020-07-08 17:38:39 +0100

    resized trader image to smooth out carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                             </ol>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img src="{{ asset('images/trader.jpg') }}" class="d-block w-100" alt="abundance">
+                                    <img src="{{ asset('images/trader-resize.jpg') }}" class="d-block w-100" alt="abundance">
                                 </div>
                                 <div class="carousel-item">
                                     <img src="{{ asset('images/carrots.jpg') }}" class="d-block w-100" alt="carrots">
